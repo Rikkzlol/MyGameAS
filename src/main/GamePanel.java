16415bc668267fc6b3211fe26e05990814862e32
Rikkzlol <89975272+Rikkzlol@users.noreply.github.com>
2023-05-05 18:01:33 +0300

    Add new game loop and fps

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -46,11 +46,13 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread.start();
     }
 
-    @Override
+    /*@Override
     public void run() {
 
         double drawInterval = 1000000000/FPS; //0.01666 sec
         double nextDrawTime = System.nanoTime() + drawInterval;
+        long timer = 0;
+        int drawCount = 0;
 
 
         while(gameThread != null){
@@ -82,8 +84,42 @@ public class GamePanel extends JPanel implements Runnable {
         }
 
 
-        
+
+    }*/
+
+    public void run() {
+
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+
+        while(gameThread != null){
+
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+
+            }
+            if(timer >= 1000000000){
+                System.out.println("FPS:"+ drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
     }
+
     public void update() {
 
         if(keyH.upPressed == true) {
