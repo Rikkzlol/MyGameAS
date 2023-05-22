ec05355d9dfa62c47bebf067f6bd8c9068955881
Rikkzlol <89975272+Rikkzlol@users.noreply.github.com>
2023-05-22 21:20:49 +0300

    add objects collision and iteration

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -14,6 +14,8 @@ public class Player extends Entity{
 
     public final int screenX;
     public final int screenY;
+    int hasKey = 0;
+
 
     public Player(GamePanel gp, KeyHandler keyH){
 
@@ -24,6 +26,8 @@ public class Player extends Entity{
         screenY = gp.screenHeight/2 - (gp.tileSize/2);
 
         solidArea = new Rectangle(8, 16, 32, 32);
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
 
         setDefaultValues();
         getPlayerImage();
@@ -71,6 +75,11 @@ public class Player extends Entity{
             collisionOn = false;
             gp.cChecker.checkTile(this);
 
+            // CHECK object collision
+            int objIndex = gp.cChecker.checkObject (this, true);
+            pickUpObject(objIndex);
+
+
             //if collision is false player can move
             if(collisionOn == false){
                 switch(direction){
@@ -91,6 +100,29 @@ public class Player extends Entity{
                 }
                 spriteCounter = 0;
             }
+        }
+    }
+    public void pickUpObject(int i){
+
+        if(i != 999){
+
+            String objectName = gp.obj[i].name;
+
+            switch (objectName){
+                case "Key":
+                    hasKey++;
+                    gp.obj[i] = null;
+                    System.out.println("Key:"+hasKey);
+                    break;
+                case "Door":
+                    if(hasKey > 0) {
+                        gp.obj[i] = null;
+                        hasKey--;w
+                    }
+                    System.out.println("Key:"+hasKey);
+                    break;
+            }
+
         }
     }
 
